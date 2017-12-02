8f603cbde2ee2fc84c1affe4901f54d3018a72b4
NEWOSURFACE\chiefnewo <chiefnwo@tpg.com.au>
2017-12-02 15:17:57 +1100

    Basic player positioning

--- Assets/scr_GameManager.cs
+++ Assets/scr_GameManager.cs
@@ -1,25 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class scr_GameManager : MonoBehaviour {
 
 	public int tileSize = 32; // size of tiles, used to move objects around the grid
+	public GameObject player; // reference to player object
+	private scr_player player_script;
+	public Tilemap tilemap; // ref to tilemap
+	private GameObject[] npcs; // npc and enemy objects
 
 	
 
 	// Use this for initialization
 	void Start () {
-		
+		Debug.Log(tilemap.origin);
+		player_script = player.GetComponent<scr_player>();
+		player_script.SetPosition(tilemap.origin.x, tilemap.origin.y);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		// Move player
+		player.transform.position = CellToWorld(tilemap.origin);
 	}
 
 	private void FixedUpdate()
 	{
 		
+	}
+
+	private Vector3 CellToWorld(Vector3Int cellRef)
+	{
+		return tilemap.GetCellCenterWorld(cellRef);
 	}
 }
--- Assets/scr_player.cs
+++ Assets/scr_player.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class scr_player : MonoBehaviour {
 
+	private int pos_x, pos_y = 0; // player grid position
+
+
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,4 +17,11 @@ public class scr_player : MonoBehaviour {
 	void Update () {
 		
 	}
+
+	public void SetPosition(int x, int y)
+	{
+		pos_x = x;
+		pos_y = y;
+	}
+
 }
